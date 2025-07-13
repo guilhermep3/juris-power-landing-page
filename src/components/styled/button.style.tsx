986b4af17819c2d846a655bc916af756370110a2
Guilherme <145510603+Guilhermep7@users.noreply.github.com>
2025-07-13 20:14:37 -0300

    initial commit

---
+++ src/components/styled/button.style.tsx
@@ -0,0 +1,10 @@
+"use client"
+import styled from "styled-components";
+
+export const Button = styled.button`
+  background-color: #DEB086;
+  width: 100px;
+  height: 40px;
+  cursor: pointer;
+
+`
